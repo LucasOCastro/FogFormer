0448b6da0fbd4c75db43348fa185eaebc7058fed
LucasOCastro <lucas_o__castro@hotmail.com>
2022-05-19 23:04:08 -0300

    Adds particles, animation and sound to dash

--- Assets/Scripts/AnimationUpdater.cs
+++ Assets/Scripts/AnimationUpdater.cs
@@ -7,8 +7,9 @@ namespace FogFormer
     {
         private static readonly int Hanging = Animator.StringToHash("hanging");
         private static readonly int Slash = Animator.StringToHash("slash");
+        private static readonly int Dash = Animator.StringToHash("dash");
         
-        [SerializeField] private PlayerController controller;
+        [SerializeField] private PlayerDasher dasher;
         [SerializeField] private WallGrabber wallGrabber;
 
         private Animator _animator;
@@ -17,19 +18,18 @@ namespace FogFormer
             _animator = GetComponent<Animator>();
         }
 
-        private void UpdateWallGrab(bool grabbing)
-        {
-            _animator.SetBool(Hanging, grabbing);
-        }
+        private void UpdateWallGrab(bool grabbing) => _animator.SetBool(Hanging, grabbing);
+        private void TriggerDash() => _animator.SetTrigger(Dash);
 
         private void OnEnable()
         {
             wallGrabber.OnWallGrabUpdate += UpdateWallGrab;
+            dasher.OnDash += TriggerDash;
         }
-
         private void OnDisable()
         {
             wallGrabber.OnWallGrabUpdate -= UpdateWallGrab;
+            dasher.OnDash -= TriggerDash;
         }
     }
 }
--- Assets/Scripts/PlayerDasher.cs
+++ Assets/Scripts/PlayerDasher.cs
@@ -4,22 +4,29 @@ namespace FogFormer
 {
     public class PlayerDasher : MonoBehaviour
     {
+        public System.Action OnDash;
+        
         [SerializeField] private float dashDistance;
         [SerializeField] private string dashButton;
         [SerializeField] LayerMask collisionMask;
 
         [SerializeField] private float cooldownSeconds;
 
+        [SerializeField] private ParticleSystem dashParticlePrefab;
+        [SerializeField] private AudioClip dashAudio;
+
         private float _cooldownTimer;
             
         private PlayerController _controller;
         private Collider2D _collider;
         private Rigidbody2D _rb;
+        private AudioSource _audio;
         private void Awake()
         {
             _controller = GetComponent<PlayerController>();
             _collider = GetComponent<Collider2D>();
             _rb = GetComponent<Rigidbody2D>();
+            _audio = GetComponent<AudioSource>();
         }
 
         private Vector2 GetEndPos(int directionSign)
@@ -29,18 +36,34 @@ namespace FogFormer
             Vector2 castOrigin = (Vector2)bounds.center + (direction * bounds.extents.x);
             var cast = Physics2D.Raycast(castOrigin, direction, dashDistance, collisionMask);
 
-            if (!cast)
+            if (cast)
             {
-                return castOrigin + direction * dashDistance;
+                Vector2 endPos = cast.point - direction * bounds.extents.x;
+                return Mathf.Approximately(endPos.x, castOrigin.x) ? bounds.center : endPos;
             }
-            return cast.point - direction * bounds.extents.x;
+            return castOrigin + direction * dashDistance;
         }
 
-        void Dash()
+        private void PlayDashEffects()
+        {
+            Instantiate(dashParticlePrefab, _rb.position, Quaternion.identity);
+            _audio.PlayOneShot(dashAudio);
+        }
+
+        private void TryDash()
         {
             int lookDirection = _controller.LookDirection;
+            Vector2 endPos = GetEndPos(lookDirection);
+
+            if (endPos == _rb.position)
+            {
+                return;
+            }
+            
             _rb.MovePosition(GetEndPos(lookDirection));
             _cooldownTimer = cooldownSeconds;
+            OnDash?.Invoke();
+            PlayDashEffects();
         }
 
         private void Update()
@@ -48,7 +71,7 @@ namespace FogFormer
             _cooldownTimer -= Time.deltaTime;
             if (_cooldownTimer <= 0 && Input.GetButtonDown(dashButton))
             {
-                Dash();
+                TryDash();
             }
         }
     }
