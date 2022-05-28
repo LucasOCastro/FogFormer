bdc8053eb0a384cbbf400ec703a37497d35b7373
LucasOCastro <lucas_o__castro@hotmail.com>
2022-05-28 10:31:33 -0300

    Enemy hit animation

--- Assets/Scripts/AI/AIAnimator.cs
+++ Assets/Scripts/AI/AIAnimator.cs
@@ -7,13 +7,17 @@ namespace FogFormer
     public class AIAnimator : MonoBehaviour
     {
         private static readonly int Walking = Animator.StringToHash("walking");
+        private static readonly int Hurt = Animator.StringToHash("hurt");
 
         private Mover _mover;
+        private HealthManager _health;
         private Animator _animator;
         private void Awake()
         {
             _animator = GetComponent<Animator>();
             _mover = GetComponent<Mover>();
+
+            _health = GetComponent<HealthManager>();
         }
 
         private void Update()
@@ -21,6 +25,19 @@ namespace FogFormer
             if (!_mover) return;
             
             _animator.SetBool(Walking, _mover.IsMoving);
+        }
+
+        private void OnDamaged() => _animator.SetTrigger(Hurt);
+
+        private void OnEnable()
+        {
+            if (_health != null)
+                _health.OnDamage += OnDamaged;
+        }
+        private void OnDisable()
+        {
+            if (_health != null)
+                _health.OnDamage -= OnDamaged;
         }
     }
 }
